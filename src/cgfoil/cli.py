ec21f2ec5e083ab9c55e9dce5e0b7bc211ec4ee6
wr1 <8971152+wr1@users.noreply.github.com>
2025-10-12 22:01:37 +0200

    Add src/cgfoil/cli.py

--- src/cgfoil/cli.py
+++ src/cgfoil/cli.py
@@ -18,12 +18,12 @@ def main():
     skins = {
         "outer_skin": {
             "thickness": np.interp(x, [0.0, 0.9, 1], [0.005, 0.005, 0.002]),
-            "material": 12,
+            "material": 2,
             "sort_index": 1,
         },
         "cap": {
             "thickness": np.interp(x, [0.2, 0.2001, 0.5, 0.5001], [0, 0.02, 0.02, 0]),
-            "material": 13,
+            "material": 1,
             "sort_index": 2,
         },
         "core": {
@@ -32,12 +32,21 @@ def main():
                 [0.05, 0.1, 0.2, 0.20001, 0.5, 0.5001, 0.7, 0.9],
                 [0, 0.01, 0.01, 0, 0, 0.02, 0.02, 0],
             ),
-            "material": 14,
+            "material": 3,
+            "sort_index": 3,
+        },
+        "teud": {
+            "thickness": np.interp(
+                x,
+                [0.7, 0.8, 0.83, 0.93],
+                [0, 0.02, 0.02, 0],
+            ),
+            "material": 3,
             "sort_index": 3,
         },
         "inner_skin": {
             "thickness": np.interp(x, [0.0, 0.9, 1], [0.005, 0.005, 0.002]),
-            "material": 15,
+            "material": 2,
             "sort_index": 4,
         },
     }
@@ -47,14 +56,14 @@ def main():
         "web1": {
             "points": ((0.25, -0.1), (0.25, 0.1)),
             "plies": [
-                {"thickness": 0.004, "material": 3},
+                {"thickness": 0.004, "material": 4},
                 {
                     "thickness": lambda y: np.interp(
                         y, [-0.04, -0.03, 0.03, 0.04], [0, 0.01, 0.01, 0]
                     ),
-                    "material": 4,
+                    "material": 3,
                 },
-                {"thickness": 0.004, "material": 3},
+                {"thickness": 0.004, "material": 4},
             ],
             "normal_ref": [1, 0],
             "n_cell": 20,
@@ -62,14 +71,14 @@ def main():
         "web2": {
             "points": ((0.4, -0.1), (0.4, 0.1)),
             "plies": [
-                {"thickness": 0.01, "material": 5},
+                {"thickness": 0.01, "material": 4},
                 {
                     "thickness": lambda y: np.interp(
                         y, [-0.04, -0.03, 0.03, 0.04], [0, 0.01, 0.01, 0]
                     ),
-                    "material": 6,
+                    "material": 3,
                 },
-                {"thickness": 0.01, "material": 7},
+                {"thickness": 0.01, "material": 4},
             ],
             "normal_ref": [-1, 0],
             "n_cell": 15,
@@ -77,12 +86,12 @@ def main():
         "web3": {
             "points": ((0.7, -0.1), (0.7, 0.1)),
             "plies": [
-                {"thickness": 0.005, "material": 5},
+                {"thickness": 0.005, "material": 4},
                 {
                     "thickness": 0.005,
-                    "material": 6,
+                    "material": 4,
                 },
-                {"thickness": 0.005, "material": 7},
+                {"thickness": 0.005, "material": 4},
             ],
             "normal_ref": [-1, 0],
             "n_cell": 15,
