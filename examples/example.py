f3949cc47570fe430cc459455b703733f46ff765
wr1 <8971152+wr1@users.noreply.github.com>
2025-10-12 09:00:33 +0200

    Add examples/example.py

---
+++ examples/example.py
@@ -0,0 +1,84 @@
+"""Programmatic example for cgfoil."""
+
+import numpy as np
+from cgfoil.core.main import run_cgfoil
+
+# Define custom skins (shell definitions)
+skins = {
+    "outer_skin": {
+        "thickness": lambda x: np.interp(x, [0.0, 0.9, 1], [0.005, 0.005, 0.002]),
+        "material": 12,
+        "sort_index": 1,
+    },
+    "cap": {
+        "thickness": lambda x: np.interp(
+            x, [0.2, 0.2001, 0.5, 0.5001], [0, 0.02, 0.02, 0]
+        ),
+        "material": 13,
+        "sort_index": 2,
+    },
+    "core": {
+        "thickness": lambda x: np.interp(
+            x,
+            [0.05, 0.1, 0.2, 0.20001, 0.5, 0.5001, 0.7, 0.9],
+            [0, 0.01, 0.01, 0, 0, 0.02, 0.02, 0],
+        ),
+        "material": 14,
+        "sort_index": 3,
+    },
+    "inner_skin": {
+        "thickness": lambda x: np.interp(x, [0.0, 0.9, 1], [0.005, 0.005, 0.002]),
+        "material": 15,
+        "sort_index": 4,
+    },
+}
+
+# Define custom web definition
+web_definition = {
+    "web1": {
+        "points": ((0.25, -0.1), (0.25, 0.1)),
+        "plies": [
+            {"thickness": 0.004, "material": 3},
+            {
+                "thickness": lambda y: np.interp(
+                    y, [-0.04, -0.03, 0.03, 0.04], [0, 0.01, 0.01, 0]
+                ),
+                "material": 4,
+            },
+            {"thickness": 0.004, "material": 3},
+        ],
+        "normal_ref": [1, 0],
+        "n_cell": 20,
+    },
+    "web2": {
+        "points": ((0.4, -0.1), (0.4, 0.1)),
+        "plies": [
+            {"thickness": 0.01, "material": 5},
+            {
+                "thickness": lambda y: np.interp(
+                    y, [-0.04, -0.03, 0.03, 0.04], [0, 0.01, 0.01, 0]
+                ),
+                "material": 6,
+            },
+            {"thickness": 0.01, "material": 7},
+        ],
+        "normal_ref": [-1, 0],
+        "n_cell": 15,
+    },
+    "web3": {
+        "points": ((0.7, -0.1), (0.7, 0.1)),
+        "plies": [
+            {"thickness": 0.005, "material": 5},
+            {
+                "thickness": 0.005,
+                "material": 6,
+            },
+            {"thickness": 0.005, "material": 7},
+        ],
+        "normal_ref": [-1, 0],
+        "n_cell": 15,
+    },
+}
+
+# Run the meshing
+run_cgfoil(skins, web_definition, plot=True, vtk="output.vtk")
