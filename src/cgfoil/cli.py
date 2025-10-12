af017ac04f0a64f2691773a22142815f5b85b670
wr1 <8971152+wr1@users.noreply.github.com>
2025-10-12 08:58:07 +0200

    Add src/cgfoil/cli.py

--- src/cgfoil/cli.py
+++ src/cgfoil/cli.py
@@ -8,7 +8,9 @@ def main():
     parser = argparse.ArgumentParser(
         description="Mesh inside NACA 0018 airfoil, its offsets as hole, and adjusted line plies with proposed web definition, normal reference, and unique material ids."
     )
-    parser.add_argument("-p", "--plot", action="store_true", help="Plot the triangulation")
+    parser.add_argument(
+        "-p", "--plot", action="store_true", help="Plot the triangulation"
+    )
     parser.add_argument("-v", "--vtk", type=str, help="Output VTK file")
     args = parser.parse_args()
     run_cgfoil(plot=args.plot, vtk=args.vtk)
